43be58ef64fc7a6fdd5efdc6879f759bb5deefef
Elito <eliot.louys@grenoble-inp.org>
2026-04-09 14:58:16 +0200

    fix: please work

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://az-fileshare-backend-hug6ake3a8amdyc7.switzerlandnorth-01.azurewebsites.net/api",
+  baseURL: "http://20.250.72.132/api",
 });
 
 api.interceptors.request.use((config) => {

a26234c8f1a6e15efb9a59a902dc71d2315a23eb
Elito <eliot.louys@grenoble-inp.org>
2026-04-09 13:30:24 +0200

    fix: api url injection

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://az-fileshare-backend-hug6ake3a8amdyc7.switzerlandnorth-01.azurewebsites.net",
+  baseURL: "https://az-fileshare-backend-hug6ake3a8amdyc7.switzerlandnorth-01.azurewebsites.net/api",
 });
 
 api.interceptors.request.use((config) => {
